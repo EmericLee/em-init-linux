3b8052b5c85f978745e859c8733af87a8c5b3d38
EmericLee <kometo@gmail.com>
2021-07-31 10:30:05 +0800

    Update init-install.sh

--- init-install.sh
+++ init-install.sh
@@ -1,6 +1,14 @@
 
+#git clone https://github.com/EmericLee/em-init-linux.git
+#mv ./em-init-linux /usr/
+#cd /usr/em-init-linux
+#./init-install.sh
+
+# Get Current path
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cp -rsfv --backup --suffix=.init.bak $DIR/home/. ~/
-cp -rsfv --backup --suffix=.init.bak $DIR/etc/. /etc/
+# Copy Config File
+cp -rfv --backup --suffix=.em~ /usr/em-init-linux/etc/. /etc/
 
+# Copy Profile 
+cp -rfv --backup --suffix=.em~ /usr/em-init-linux/home/. ~/
